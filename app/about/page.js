4e983edb93ebc1e3a38a32a94ad23b8a066ab255
mosaro404@gmail.com <mosaro404@gmail.com>
2025-05-18 23:37:52 +0200

    dd

--- app/about/page.js
+++ app/about/page.js
@@ -25,11 +25,7 @@ const About = () => {
 
   return (
 
-  <div style={{
-        background: 'linear-gradient(135deg, rgba(161, 208, 241, 0.41), rgba(255, 255, 255, 0.4), rgba(255, 255, 255, 0.4))',
-        backgroundColor: 'white',
-        boxShadow: '0px 4px 20px rgba(116, 74, 74, 0.1)',
-      }}>
+  <div>
     <div id="it" className={`px-5 py-10 min-h-screen flex flex-col items-center justify-center text-center ${ovo.className}`}>
       <div className="mb-10">
         <h4 className="sm:text-[18px] md:text-[20px] lg:text-[30px]">Introduction</h4>
@@ -41,7 +37,7 @@ const About = () => {
           src={assets.about_pic}
           alt="About Me"
           className="rounded-3xl"
-          width={650}
+          width={350}
           height={400}
           style={{
             borderRadius: '1.5rem',
@@ -51,7 +47,7 @@ const About = () => {
       </div>
 
       <p className="sm:text-[18px] md:text-[25px] lg:text-[40px] max-w-4xl text-justify mb-10"
-      style={{marginRight:'-5rem', marginLeft:'-5rem'}}>
+      style={{marginRight:'2rem', marginLeft:'2rem'}}>
         I'm a front-end web developer from South Africa, skilled in HTML, CSS, JavaScript, and React.
         I create clean, responsive websites, focusing on both function and design. I enjoy building interactive
         digital experiences that people love using.
